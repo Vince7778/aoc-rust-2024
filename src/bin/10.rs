a641bbb92fdd817e0d05da011ef7568f7a214df6
Conor Kennedy <realvince7778@gmail.com>
2024-12-09 21:17:25 -0800

    day 10 cleaned

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,14 +1,16 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 
 use advent_of_code::{grid, neighbors, parse_char};
 
 advent_of_code::solution!(10);
 
-fn dfs(g: &Vec<Vec<char>>, r: usize, c: usize, vis: &mut HashSet<(usize, usize)>) -> usize {
-    if vis.contains(&(r, c)) {
-        return 0;
+fn dfs(g: &Vec<Vec<char>>, r: usize, c: usize, vis: &mut Option<HashSet<(usize, usize)>>) -> usize {
+    if let Some(vis) = vis {
+        if vis.contains(&(r, c)) {
+            return 0;
+        }
+        vis.insert((r, c));
     }
-    vis.insert((r, c));
     if g[r][c] == '9' {
         return 1;
     }
@@ -27,29 +29,12 @@ pub fn part_one(input: &str) -> Option<u32> {
     for sr in 0..g.len() {
         for sc in 0..g[0].len() {
             if g[sr][sc] == '0' {
-                let mut vis = HashSet::new();
-                ans += dfs(&g, sr, sc, &mut vis);
+                let vis = HashSet::new();
+                ans += dfs(&g, sr, sc, &mut Some(vis));
             }
         }
     }
     Some(ans as u32)
-}
-
-fn dfs2(g: &Vec<Vec<char>>, r: usize, c: usize, dp: &mut HashMap<(usize, usize), usize>) -> usize {
-    if dp.contains_key(&(r, c)) {
-        return *dp.get(&(r, c)).unwrap();
-    }
-    if g[r][c] == '9' {
-        return 1;
-    }
-    let mut out = 0;
-    for (nr, nc, _) in neighbors(r, c, g.len(), g[0].len()) {
-        if parse_char(g[nr][nc]) == parse_char(g[r][c]) + 1 {
-            out += dfs2(g, nr, nc, dp);
-        }
-    }
-    dp.insert((r, c), out);
-    out
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
@@ -58,8 +43,7 @@ pub fn part_two(input: &str) -> Option<u32> {
     for sr in 0..g.len() {
         for sc in 0..g[0].len() {
             if g[sr][sc] == '0' {
-                let mut vis = HashMap::new();
-                ans += dfs2(&g, sr, sc, &mut vis);
+                ans += dfs(&g, sr, sc, &mut None);
             }
         }
     }

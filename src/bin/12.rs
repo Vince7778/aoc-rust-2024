af5f730f3f839cad36801815b8e01fcf13bd7b78
Conor Kennedy <realvince7778@gmail.com>
2024-12-11 21:37:00 -0800

    day 12 formatted

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -2,7 +2,13 @@ use advent_of_code::{grid, neighbors, repeat_2d};
 
 advent_of_code::solution!(12);
 
-fn dfs(r: usize, c: usize, peri: &Vec<Vec<usize>>, g: &Vec<Vec<char>>, vis: &mut Vec<Vec<bool>>) -> (usize, usize) {
+fn dfs(
+    r: usize,
+    c: usize,
+    peri: &Vec<Vec<usize>>,
+    g: &Vec<Vec<char>>,
+    vis: &mut Vec<Vec<bool>>,
+) -> (usize, usize) {
     if vis[r][c] {
         return (0, 0);
     }
@@ -27,16 +33,16 @@ pub fn part_one(input: &str) -> Option<usize> {
     let mut peri = repeat_2d(0_usize, g.len(), g[0].len());
     for r in 0..g.len() {
         for c in 0..g[0].len() {
-            if r == 0 || g[r][c] != g[r-1][c] {
+            if r == 0 || g[r][c] != g[r - 1][c] {
                 peri[r][c] += 1;
             }
-            if c == 0 || g[r][c] != g[r][c-1] {
+            if c == 0 || g[r][c] != g[r][c - 1] {
                 peri[r][c] += 1;
             }
-            if r == g.len()-1 || g[r][c] != g[r+1][c] {
+            if r == g.len() - 1 || g[r][c] != g[r + 1][c] {
                 peri[r][c] += 1;
             }
-            if c == g[0].len()-1 || g[r][c] != g[r][c+1] {
+            if c == g[0].len() - 1 || g[r][c] != g[r][c + 1] {
                 peri[r][c] += 1;
             }
         }
@@ -46,7 +52,7 @@ pub fn part_one(input: &str) -> Option<usize> {
     for r in 0..g.len() {
         for c in 0..g[0].len() {
             let (a, p) = dfs(r, c, &peri, &g, &mut vis);
-            ans += a*p;
+            ans += a * p;
         }
     }
     Some(ans)
@@ -58,16 +64,28 @@ pub fn part_two(input: &str) -> Option<usize> {
     let mut peri = repeat_2d(0_usize, g.len(), g[0].len());
     for r in 0..g.len() {
         for c in 0..g[0].len() {
-            if (r == 0 || g[r][c] != g[r-1][c]) && (c == 0 || g[r][c] != g[r][c-1] || (r != 0 && g[r][c-1] == g[r-1][c-1])) {
+            if (r == 0 || g[r][c] != g[r - 1][c])
+                && (c == 0 || g[r][c] != g[r][c - 1] || (r != 0 && g[r][c - 1] == g[r - 1][c - 1]))
+            {
                 peri[r][c] += 1;
             }
-            if (c == 0 || g[r][c] != g[r][c-1]) && (r == 0 || g[r][c] != g[r-1][c] || (c != 0 && g[r-1][c] == g[r-1][c-1])) {
+            if (c == 0 || g[r][c] != g[r][c - 1])
+                && (r == 0 || g[r][c] != g[r - 1][c] || (c != 0 && g[r - 1][c] == g[r - 1][c - 1]))
+            {
                 peri[r][c] += 1;
             }
-            if (r == g.len()-1 || g[r][c] != g[r+1][c]) && (c == 0 || g[r][c] != g[r][c-1] || (r != g.len()-1 && g[r][c-1] == g[r+1][c-1])) {
+            if (r == g.len() - 1 || g[r][c] != g[r + 1][c])
+                && (c == 0
+                    || g[r][c] != g[r][c - 1]
+                    || (r != g.len() - 1 && g[r][c - 1] == g[r + 1][c - 1]))
+            {
                 peri[r][c] += 1;
             }
-            if (c == g[0].len()-1 || g[r][c] != g[r][c+1]) && (r == 0 || g[r][c] != g[r-1][c] || (c != g[0].len()-1 && g[r-1][c] == g[r-1][c+1])) {
+            if (c == g[0].len() - 1 || g[r][c] != g[r][c + 1])
+                && (r == 0
+                    || g[r][c] != g[r - 1][c]
+                    || (c != g[0].len() - 1 && g[r - 1][c] == g[r - 1][c + 1]))
+            {
                 peri[r][c] += 1;
             }
         }
@@ -78,7 +96,7 @@ pub fn part_two(input: &str) -> Option<usize> {
     for r in 0..g.len() {
         for c in 0..g[0].len() {
             let (a, p) = dfs(r, c, &peri, &g, &mut vis);
-            ans += a*p;
+            ans += a * p;
         }
     }
     Some(ans)
@@ -92,9 +110,13 @@ mod tests {
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(140));
-        let result = part_one(&advent_of_code::template::read_file_part("examples", DAY, 2));
+        let result = part_one(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
         assert_eq!(result, Some(772));
-        let result = part_one(&advent_of_code::template::read_file_part("examples", DAY, 3));
+        let result = part_one(&advent_of_code::template::read_file_part(
+            "examples", DAY, 3,
+        ));
         assert_eq!(result, Some(1930));
     }
 
@@ -102,13 +124,21 @@ mod tests {
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(80));
-        let result = part_two(&advent_of_code::template::read_file_part("examples", DAY, 2));
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
         assert_eq!(result, Some(436));
-        let result = part_two(&advent_of_code::template::read_file_part("examples", DAY, 3));
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 3,
+        ));
         assert_eq!(result, Some(1206));
-        let result = part_two(&advent_of_code::template::read_file_part("examples", DAY, 4));
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 4,
+        ));
         assert_eq!(result, Some(236));
-        let result = part_two(&advent_of_code::template::read_file_part("examples", DAY, 5));
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 5,
+        ));
         assert_eq!(result, Some(368));
     }
 }

a15f80c90b5b43ca1dc39b5651db2362d85251f1
Conor Kennedy <realvince7778@gmail.com>
2024-12-16 17:20:59 -0500

    day 16 formatted

--- src/bin/16.rs
+++ src/bin/16.rs
@@ -4,12 +4,13 @@ use advent_of_code::{grid, run_move, DD};
 
 advent_of_code::solution!(16);
 
+type PosDir = ((usize, usize), usize);
 pub fn part_one(input: &str) -> Option<usize> {
     let g = grid(input);
-    let s = (g.len()-2, 1);
-    let e = (1, g[0].len()-2);
-    
-    let mut dv: HashMap<((usize, usize), usize), isize> = HashMap::new();
+    let s = (g.len() - 2, 1);
+    let e = (1, g[0].len() - 2);
+
+    let mut dv: HashMap<PosDir, isize> = HashMap::new();
     let mut pq: BinaryHeap<(isize, (usize, usize), usize)> = BinaryHeap::new();
     pq.push((0, s, 1));
     while let Some((dist, p, d)) = pq.pop() {
@@ -21,9 +22,9 @@ pub fn part_one(input: &str) -> Option<usize> {
             continue;
         }
         let nxt = vec![
-            (dist+1, run_move(p, DD[d]), d),
-            (dist+1000, p, (d+1)%4),
-            (dist+1000, p, (d+3)%4),
+            (dist + 1, run_move(p, DD[d]), d),
+            (dist + 1000, p, (d + 1) % 4),
+            (dist + 1000, p, (d + 3) % 4),
         ];
         for (ndist, np, nd) in nxt {
             if g[np.0][np.1] != '#' && !dv.get(&(np, nd)).is_some_and(|&cd| cd < ndist) {
@@ -37,13 +38,13 @@ pub fn part_one(input: &str) -> Option<usize> {
 
 pub fn part_two(input: &str) -> Option<usize> {
     let g = grid(input);
-    let s = (g.len()-2, 1);
-    let e = (1, g[0].len()-2);
-    
-    let mut dv: HashMap<((usize, usize), usize), isize> = HashMap::new();
+    let s = (g.len() - 2, 1);
+    let e = (1, g[0].len() - 2);
+
+    let mut dv: HashMap<PosDir, isize> = HashMap::new();
     let mut de = isize::MAX;
     let mut pq: BinaryHeap<(isize, (usize, usize), usize)> = BinaryHeap::new();
-    let mut prevs: HashMap<((usize, usize), usize), Vec<((usize, usize), usize)>> = HashMap::new();
+    let mut prevs: HashMap<PosDir, Vec<PosDir>> = HashMap::new();
     pq.push((0, s, 1));
     while let Some((dist, p, d)) = pq.pop() {
         let dist = -dist;
@@ -57,9 +58,9 @@ pub fn part_two(input: &str) -> Option<usize> {
             continue;
         }
         let nxt = vec![
-            (dist+1, run_move(p, DD[d]), d),
-            (dist+1000, p, (d+1)%4),
-            (dist+1000, p, (d+3)%4),
+            (dist + 1, run_move(p, DD[d]), d),
+            (dist + 1000, p, (d + 1) % 4),
+            (dist + 1000, p, (d + 3) % 4),
         ];
         for (ndist, np, nd) in nxt {
             if g[np.0][np.1] != '#' && !dv.get(&(np, nd)).is_some_and(|&cd| cd < ndist) {
@@ -73,8 +74,8 @@ pub fn part_two(input: &str) -> Option<usize> {
         }
     }
     let mut spots: HashSet<(usize, usize)> = HashSet::new();
-    let mut vis: HashSet<((usize, usize), usize)> = HashSet::new();
-    let mut stk: Vec<((usize, usize), usize)> = Vec::new();
+    let mut vis: HashSet<PosDir> = HashSet::new();
+    let mut stk: Vec<PosDir> = Vec::new();
     for i in 0..4 {
         stk.push((e, i));
         vis.insert((e, i));
@@ -101,7 +102,9 @@ mod tests {
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(7036));
-        let result = part_one(&advent_of_code::template::read_file_part("examples", DAY, 2));
+        let result = part_one(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
         assert_eq!(result, Some(11048));
     }
 
@@ -109,7 +112,9 @@ mod tests {
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(45));
-        let result = part_two(&advent_of_code::template::read_file_part("examples", DAY, 2));
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
         assert_eq!(result, Some(64));
     }
 }
